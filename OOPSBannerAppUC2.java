91eeb2357a29de35365974ee52a73d8a0405c5f5
Priyan <pm1718@srmist.edu.in>
2026-03-03 09:03:33 +0530

    Add UC1 & UC2

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerAppUC2
+ *
+ * Displays "OOPS" as a large ASCII banner.
+ *
+ * @author Priyan
+ * @version 2.0
+ */
+public class OOPSBannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    *****  ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println(" *****    *****    *        *****  ");
+    }
+}
